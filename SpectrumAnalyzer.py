4fe0ed412b6b2d09b40351ee98fd49e135eda8b9
huazhilun <huazhi.lun@sjtu.edu.cn>
2020-01-15 04:40:23 +0800

    finshed

---
+++ SpectrumAnalyzer.py
@@ -0,0 +1,126 @@
+from PyQt5.QtWidgets import QWidget
+from SpectrunUi import Ui_Form
+from PyQt5.QtWidgets import QApplication,QFileDialog,QMessageBox
+from PyQt5.QtCore import pyqtSlot,QThread,pyqtSignal
+import numpy as np
+import pyqtgraph as pg
+class CalculateSpectrum(QThread):
+
+    calc_finshed = pyqtSignal(np.ndarray,np.ndarray)
+
+    def __init__(self,data,fs,fft_point,window):
+        super(CalculateSpectrum, self).__init__()
+        self.data = data
+        self.fs = fs
+        self.fft_point = fft_point
+        self.window = window
+
+
+    def run(self) -> None:
+
+        self.data = np.atleast_2d(self.data)
+        pol_number = self.data.shape[0]
+        spectrum_sequence = np.zeros((pol_number,int(self.fft_point)),dtype=self.data.dtype)
+
+        from scipy.signal import welch
+        for i in range(pol_number):
+            f,p = welch(self.data[i], self.fs, window=self.window,nfft=self.fft_point,return_onesided=False,detrend=False)
+            spectrum_sequence[i] = p
+        spectrum_sequence = 10*np.log10(np.abs(spectrum_sequence))
+        self.calc_finshed.emit(f,spectrum_sequence)
+
+class SpectrumAnalyzer(QWidget):
+    def __init__(self,parent=None):
+        super(SpectrumAnalyzer, self).__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        self.data = None
+        self.ui.AnalyzeButton.clicked[bool].connect(self.analyze_spectrum)
+
+        self.analyzer = None
+
+    @pyqtSlot(bool)
+    def analyze_spectrum(self,is_clicked):
+        try:
+            if self.ui.fs.text():
+                fs = float(self.ui.fs.text())
+            if self.ui.FFTPoint.text():
+                fft_point = float(self.ui.FFTPoint.text())
+            chidu = self.ui.Ychidu.currentText()
+            self.analyzer = CalculateSpectrum(self.data, fs, fft_point, window='hann')
+            self.analyzer.calc_finshed.connect(self.plot_spectrum)
+            self.analyzer.start()
+
+        except Exception as e:
+            QMessageBox.warning(None,'error',str(e),QMessageBox.Yes)
+            return
+
+
+    @pyqtSlot(np.ndarray,np.ndarray)
+    def plot_spectrum(self,f,spectrum):
+        f = np.atleast_2d(f)
+        spectrum = np.atleast_2d(spectrum)
+        f = f[0]
+        self.ui.graphicsView.clear()
+        for i in range(spectrum.shape[0]):
+            w1 = self.ui.graphicsView.addPlot()
+            w1.plot(f,spectrum[i],pen=pg.mkPen('r', width=3))
+            self.ui.graphicsView.nextRow()
+
+        self.analyzer = None
+
+    @pyqtSlot(bool)
+    def on_OpenFileButton_clicked(self,is_clicked):
+        # print('xixi')
+        fname,_ = QFileDialog.getOpenFileName(self,'open files','./','data (*.npz *.mat)')
+        try:
+            if fname.endswith('.npz'):
+                import numpy as np
+                self.data = np.load(fname)['arr_0']
+
+            if fname.endswith('.mat'):
+                from scipy.io import loadmat
+                self.data = loadmat(fname)['samples']
+        except Exception as e:
+            QMessageBox.warning(None, 'error', 'open file error'+str(e), QMessageBox.Yes)
+
+
+
+        try:
+            self.data = np.atleast_2d(self.data)[:,:2**16]
+        except Exception as e:
+            QMessageBox.warning(None, 'error', str(e), QMessageBox.Yes)
+            return
+
+    @pyqtSlot(bool)
+    def on_ClearFig_clicked(self, is_clicked):
+        self.ui.graphicsView.clear()
+
+
+
+
+
+import numpy as np
+class ceshi(QWidget):
+    signal = pyqtSignal(np.ndarray)
+
+    def emit(self):
+        self.signal.emit(np.array([1,2,3]))
+
+    def slot(self,array):
+        # print(array)
+        print(type(array))
+
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    analyzer = SpectrumAnalyzer()
+    analyzer.show()
+    #
+    # c = ceshi()
+    # c.signal.connect(c.slot)
+    # c.emit()
+    sys.exit(app.exec())
---
+++ SpectrunUi.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(881, 641)
+        self.gridLayout_3 = QtWidgets.QGridLayout(Form)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.graphicsView = GraphicsLayoutWidget(Form)
+        self.graphicsView.setObjectName("graphicsView")
+        self.verticalLayout.addWidget(self.graphicsView)
+
+        self.gridLayout_3.addLayout(self.verticalLayout, 0, 0, 2, 1)
+        self.OpenFileButton = QtWidgets.QPushButton(Form)
+        self.OpenFileButton.setObjectName("OpenFileButton")
+        self.gridLayout_3.addWidget(self.OpenFileButton, 0, 1, 1, 1)
+        self.ConfigFrame = QtWidgets.QFrame(Form)
+        self.ConfigFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.ConfigFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.ConfigFrame.setObjectName("ConfigFrame")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.ConfigFrame)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        self.AnalyzeButton = QtWidgets.QPushButton(self.ConfigFrame)
+        self.AnalyzeButton.setObjectName("AnalyzeButton")
+        self.gridLayout.addWidget(self.AnalyzeButton, 3, 0, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.ConfigFrame)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.save = QtWidgets.QPushButton(self.ConfigFrame)
+        self.save.setObjectName("save")
+        self.gridLayout.addWidget(self.save, 3, 2, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.ConfigFrame)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+        self.FFTPoint = QtWidgets.QLineEdit(self.ConfigFrame)
+        self.FFTPoint.setObjectName("FFTPoint")
+        self.gridLayout.addWidget(self.FFTPoint, 0, 2, 1, 1)
+        self.Ychidu = QtWidgets.QComboBox(self.ConfigFrame)
+        self.Ychidu.setObjectName("Ychidu")
+        self.Ychidu.addItem("")
+        self.Ychidu.addItem("")
+        self.gridLayout.addWidget(self.Ychidu, 1, 2, 1, 1)
+        self.label = QtWidgets.QLabel(self.ConfigFrame)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.fs = QtWidgets.QLineEdit(self.ConfigFrame)
+        self.fs.setObjectName("fs")
+        self.gridLayout.addWidget(self.fs, 2, 2, 1, 1)
+        self.ClearFig = QtWidgets.QPushButton(self.ConfigFrame)
+        self.ClearFig.setObjectName("ClearFig")
+        self.gridLayout.addWidget(self.ClearFig, 3, 1, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.ConfigFrame, 1, 1, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.OpenFileButton.setText(_translate("Form", "打开文件"))
+        self.AnalyzeButton.setText(_translate("Form", "Analyze"))
+        self.label_2.setText(_translate("Form", "Log/Linear"))
+        self.save.setText(_translate("Form", "save"))
+        self.label_3.setText(_translate("Form", "采样频率 Hz"))
+        self.Ychidu.setItemText(0, _translate("Form", "linear"))
+        self.Ychidu.setItemText(1, _translate("Form", "log"))
+        self.label.setText(_translate("Form", "FFT point"))
+        self.ClearFig.setText(_translate("Form", "clear"))
+from pyqtgraph import GraphicsLayoutWidget
